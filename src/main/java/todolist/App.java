d89aba76b5e003e534915d7f3a77252030a6682c
Pop Adriana <anni.ania19@yahoo.com>
2019-10-21 20:05:19 +0300

    to do items in repository

--- src/main/java/todolist/App.java
+++ src/main/java/todolist/App.java
@@ -1,26 +1,30 @@
 package todolist;
 
+import todolist.domain.ToDoItem;
 import todolist.persistance.ToDoItemRepository;
 import todolist.transfer.CreatedToDoItemRequest;
 
 import java.io.IOException;
 import java.sql.SQLException;
 import java.time.LocalDate;
+import java.util.List;
 
 /**
  * Hello world!
- *
  */
-public class App 
-{
-    public static void main( String[] args ) throws SQLException, IOException, ClassNotFoundException {
+public class App {
+    public static void main(String[] args) throws SQLException, IOException, ClassNotFoundException {
 
         CreatedToDoItemRequest request = new CreatedToDoItemRequest();
         request.setDescription("Learn JDBC");
         request.setDeadline(LocalDate.now().plusWeeks(1));
 
         ToDoItemRepository toDoItemRepository = new ToDoItemRepository();
-        toDoItemRepository.createToDoItem(request);
+        // toDoItemRepository.createToDoItem(request);
+        // toDoItemRepository.updateToDoItem(1, true);
+        // toDoItemRepository.deleteToDoItem(1);
+        List<ToDoItem> toDoItems = toDoItemRepository.getToDoItems();
 
+        System.out.println(toDoItems);
     }
 }
--- src/main/java/todolist/persistance/ToDoItemRepository.java
+++ src/main/java/todolist/persistance/ToDoItemRepository.java
@@ -1,12 +1,12 @@
 package todolist.persistance;
 
+import todolist.domain.ToDoItem;
 import todolist.transfer.CreatedToDoItemRequest;
 
 import java.io.IOException;
-import java.sql.Connection;
-import java.sql.Date;
-import java.sql.PreparedStatement;
-import java.sql.SQLException;
+import java.sql.*;
+import java.util.ArrayList;
+import java.util.List;
 
 public class ToDoItemRepository {
     public void createToDoItem(CreatedToDoItemRequest request) throws SQLException, IOException, ClassNotFoundException {
@@ -20,4 +20,50 @@ public class ToDoItemRepository {
         }
 
     }
+
+    public void updateToDoItem(long id, boolean done) throws SQLException, IOException, ClassNotFoundException {
+        String sql = "UPDATE  to_do_item SET done=? WHERE id=? ";
+        try (Connection connection = DatabaseConfiguration.getConnection();
+             PreparedStatement preparedStatement = connection.prepareStatement(sql)) {
+            preparedStatement.setBoolean(1, done);
+            preparedStatement.setLong(2, id);
+
+            preparedStatement.executeUpdate();
+        }
+    }
+
+
+    public void deleteToDoItem(long id) throws SQLException, IOException, ClassNotFoundException {
+
+        String sql = "DELETE FROM to_do_item WHERE id=? ";
+
+        try (Connection connection = DatabaseConfiguration.getConnection();
+             PreparedStatement preparedStatement = connection.prepareStatement(sql)) {
+            preparedStatement.setLong(1, id);
+            preparedStatement.executeUpdate();
+        }
+    }
+
+    public List<ToDoItem> getToDoItems() throws SQLException, IOException, ClassNotFoundException {
+        String sql = "SELECT id, ide, description, deadline, done FROM to_do_item ";
+
+        List<ToDoItem> toDoItems = new ArrayList<>();
+
+        try (Connection connection = DatabaseConfiguration.getConnection();
+             Statement statement = connection.createStatement();
+             ResultSet resultSet = statement.executeQuery(sql)) {
+            while (resultSet.next()) {
+                ToDoItem toDoItem = new ToDoItem();
+                toDoItem.setId(resultSet.getLong("id"));
+                toDoItem.setDescription(resultSet.getString("description"));
+                toDoItem.setDeadline(resultSet.getDate("deadline").toLocalDate());
+                toDoItem.setDone(resultSet.getBoolean("done"));
+
+                toDoItems.add(toDoItem);
+            }
+
+        }
+        return toDoItems;
+    }
 }
+
